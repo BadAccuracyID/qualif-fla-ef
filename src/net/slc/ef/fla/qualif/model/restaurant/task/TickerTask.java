b9282888231acf531635393e553840b0887524b5
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-08-28 12:36:13 +0700

    Improved TickerTask printing

--- src/net/slc/ef/fla/qualif/model/restaurant/task/TickerTask.java
+++ src/net/slc/ef/fla/qualif/model/restaurant/task/TickerTask.java
@@ -21,32 +21,31 @@ public class TickerTask implements Runnable {
     public void run() {
         // print UI
         Utils.clearScreen();
-        System.out.println("----- " + restaurant.getName() + " -----");
+        System.out.println("                    " + restaurant.getName() + " Restaurant is now open!");
         System.out.println();
-        System.out.println("        Status      ");
-        System.out.printf("     Money: Rp. %d\n", restaurant.getMoney());
-        System.out.printf("     Score: %d Points\n", restaurant.getScore());
-        System.out.printf("     Size: %d Seats\n", restaurant.getChairs().size());
+        System.out.println("                            Status");
+        System.out.printf("                         Money  : Rp. %d\n", restaurant.getMoney());
+        System.out.printf("                         Score  : %d Points\n", restaurant.getScore());
+        System.out.printf("                         Size   : %d Seats\n", restaurant.getChairs().size());
         System.out.println();
         System.out.println("==================================================================================");
         System.out.println("          Customers          |          Waiters          |         Cooks          ");
         System.out.println("==================================================================================");
 
-        for (int i = 0; i < restaurant.getRestaurantFacade().getPersons().size(); i++) {
-            // <initial> <Patience> <Status><Server> | <Initial>,<Status><Cook> | <Initial>,<Status><Customer>
+        for (int i = 0; i < getLinesNeeded(); i++) {
             Customer customer;
             if ((customer = this.getCustomer(restaurant.getRestaurantFacade().getCustomers(), i)) != null) {
                 System.out.printf("%-2s <%-2d>,%-5s<%-2s>", customer.getInitial(), customer.getTolerance(), "wait food", customer.getInitial());
             } else {
-                System.out.print("                          ");
+                System.out.print("                            ");
             }
             System.out.print(" | ");
 
             Waiter waiter;
             if ((waiter = this.getWaiter(restaurant.getWaiters(), i)) != null) {
-                System.out.printf("<%-2s>, %17s<%-2s>", waiter.getInitial(), "take order", waiter.getInitial());
+                System.out.printf("<%-2s>, %15s<%-2s>", waiter.getInitial(), "take order", waiter.getInitial());
             } else {
-                System.out.print("                           ");
+                System.out.print("                         ");
             }
             System.out.print(" | ");
 
@@ -60,12 +59,24 @@ public class TickerTask implements Runnable {
             System.out.println();
         }
 
+        System.out.println();
+        System.out.println("                         Press enter to pause the game ");
+
         // tick all persons
         if (restaurant.getRestaurantFacade().getPersons().isEmpty()) {
             return;
         }
 
         restaurant.getRestaurantFacade().getPersons().forEach(AbstractPerson::tick);
+    }
+
+    private int getLinesNeeded() {
+        // so, each customer is 1 line, and each waiter and cook is 1/2 line
+        int personSize = restaurant.getRestaurantFacade().getCustomers().size();
+        int waiterSize = (int) Math.ceil(restaurant.getWaiters().size() / 2.0);
+        int cookSize = (int) Math.ceil(restaurant.getCooks().size() / 2.0);
+
+        return personSize + waiterSize + cookSize;
     }
 
     private Customer getCustomer(List<Customer> list, int index) {
